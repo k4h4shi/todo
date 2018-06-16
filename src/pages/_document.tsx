347e47cf76551de68ae12f74976b20152c544de0
kotaro takahashi <kotaro.t@k4h4shi.com>
2018-06-16 22:29:41 +0900

    styled-componentsを導入

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,11 +3,17 @@
 
 // ./pages/_document.js
 import Document, { Head, Main, NextScript } from "next/document";
+import { ServerStyleSheet } from "styled-components";
 
 export default class MyDocument extends Document {
   static async getInitialProps(ctx) {
     const initialProps = await Document.getInitialProps(ctx);
-    return { ...initialProps };
+    const sheet = new ServerStyleSheet();
+    const page = ctx.renderPage(App => props =>
+      sheet.collectStyles(<App {...props} />)
+    );
+    const styleTags = sheet.getStyleElement();
+    return { ...initialProps, ...page, styleTags };
   }
 
   render() {
@@ -88,6 +94,7 @@ export default class MyDocument extends Document {
               text-decoration: none;
             }
           `}</style>
+          {this.props.styleTags}
         </Head>
         <body>
           <Main />

532160e6e8adac19f6ba6df8db0dc3e82f0dbb88
kotaro takahashi <kotaro.t@k4h4shi.com>
2018-06-16 21:13:17 +0900

    ./pagesと./serverを./src直下に移動

--- src/server/index.ts
+++ src/server/index.ts
@@ -6,7 +6,7 @@ config();
 const port = parseInt(process.env.PORT, 10) || 3000;
 const dev = process.env.NODE_ENV !== "production";
 
-const app = next({ dev });
+const app = next({ dir: "./src", dev });
 const handle = app.getRequestHandler();
 
 app.prepare().then(() => {

09eecb938e12ef1f0cb92a1d8c858708bcb6d833
kotaro takahashi <kotaro.t@k4h4shi.com>
2018-06-20 03:32:31 +0900

    Todo作成と、Todo更新のエンドポイントを追加

--- src/server/controllers/todolists.ts
+++ src/server/controllers/todolists.ts
@@ -1,6 +1,12 @@
 import { Request, Response, NextFunction } from "express";
 import TodoList, { ITodoList } from "../models/TodoList";
 
+/**
+ * Todoリストの一覧を取得
+ * @param _req
+ * @param res
+ * @param next
+ */
 export const index = (_req: Request, res: Response, next: NextFunction) => {
   TodoList.find().exec((err: Error, todoLists: ITodoList[]) => {
     if (err) {
@@ -10,6 +16,12 @@ export const index = (_req: Request, res: Response, next: NextFunction) => {
   });
 };
 
+/**
+ * 任意のTodoリストを取得
+ * @param req
+ * @param res
+ * @param next
+ */
 export const show = (req: Request, res: Response, next: NextFunction) => {
   const { id } = req.params;
   TodoList.findById(id).exec((err: Error, todoList: ITodoList) => {
@@ -20,6 +32,12 @@ export const show = (req: Request, res: Response, next: NextFunction) => {
   });
 };
 
+/**
+ * Todoリストを作成
+ * @param req
+ * @param res
+ * @param next
+ */
 export const create = (req: Request, res: Response, next: NextFunction) => {
   const body = req.body;
   const todoList = new TodoList(body);
@@ -28,5 +46,58 @@ export const create = (req: Request, res: Response, next: NextFunction) => {
       return next(err);
     }
     res.status(201).json(todoList);
+  });
+};
+
+/**
+ * TodoリストにTodoを追加
+ * @param req
+ * @param res
+ * @param next
+ */
+export const createTodo = (req: Request, res: Response, next: NextFunction) => {
+  const { id } = req.params;
+  const { name, due } = req.body;
+  TodoList.findOneAndUpdate(
+    { _id: id },
+    {
+      $push: {
+        todos: {
+          name,
+          due
+        }
+      }
+    },
+    { new: true }
+  ).exec((err: Error, todoList: ITodoList) => {
+    if (err) {
+      return next(err);
+    }
+    const added = todoList.todos[todoList.todos.length - 1];
+    res.status(201).json(added);
+  });
+};
+
+/**
+ * TodoリストのTodoを更新
+ * @param req
+ * @param res
+ * @param next
+ */
+export const updateTodo = (req: Request, res: Response, next: NextFunction) => {
+  const { id, todoId } = req.params;
+  const { body } = req;
+  TodoList.findOneAndUpdate(
+    { _id: id, "todos._id": todoId },
+    {
+      $set: {
+        "todos.$": { ...body, _id: todoId }
+      }
+    }
+  ).exec((err: Error, _todoList: ITodoList) => {
+    if (err) {
+      return next(err);
+    }
+    res.sendStatus(200);
   });
 };
--- src/server/routers/todolists.ts
+++ src/server/routers/todolists.ts
@@ -6,5 +6,7 @@ const router = Router();
 router.get("/", todoLists.index);
 router.get("/:id", todoLists.show);
 router.post("/", todoLists.create);
+router.post("/:id/todos/", todoLists.createTodo);
+router.put("/:id/todos/:todoId", todoLists.updateTodo);
 
 export default router;

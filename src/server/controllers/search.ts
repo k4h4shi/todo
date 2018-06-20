7afb3a2b28991ed3a7f7b9ff930de1ef878ff4a3
kotaro takahashi <kotaro.t@k4h4shi.com>
2018-06-20 21:36:03 +0900

    検索のエンドポイントを実装

---
+++ src/server/controllers/search.ts
@@ -0,0 +1,34 @@
+import { Request, Response, NextFunction } from "express";
+import TodoList, { ITodoList } from "../models/TodoList";
+import Todo, { ITodo } from "../models/Todo";
+
+/**
+ * 検索結果を取得
+ * TodoリストとTodoに対し、リクエストのクエリの文字列でLIKE検索する。
+ * @param _req
+ * @param res
+ * @param next
+ */
+export const index = (req: Request, res: Response, next: NextFunction) => {
+  console.log(req.query);
+  const { q } = req.query;
+
+  Promise.all([
+    TodoList.find({ name: new RegExp(`${q}`) })
+      .populate("todos")
+      .exec(),
+    Todo.find({ name: new RegExp(`${q}`) })
+      .populate("todoList")
+      .exec()
+  ])
+    .then((results: [ITodoList[], ITodo[]]) => {
+      const [todoLists, todos] = results;
+      res.status(200).json({
+        todoLists,
+        todos
+      });
+    })
+    .catch(err => {
+      next(err);
+    });
+};

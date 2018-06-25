c9a8bf389ca76537b55cd7889b28d323b958b06f
kotaro takahashi <kotaro.t@k4h4shi.com>
2018-06-26 01:39:16 +0900

    トップページのTodoリストの並び順を修正
    ToDoリストに登録されているToDoの作成日が新しい順に表示するように変更

--- src/server/controllers/todolists.ts
+++ src/server/controllers/todolists.ts
@@ -9,12 +9,19 @@ import TodoList, { ITodoList } from "../models/TodoList";
  */
 export const index = (_req: Request, res: Response, next: NextFunction) => {
   TodoList.find()
-    .populate("todos")
+    // Todoリストに紐付くtodoを作成日が新しい順に並び替えた上で連結する
+    .populate({
+      path: "todos",
+      options: {
+        sort: "-createdAt"
+      }
+    })
     .exec((err: Error, todoLists: ITodoList[]) => {
       if (err) {
         return next(err);
       }
-      res.json(todoLists);
+      const sorted = sortTodoLists(todoLists);
+      res.json(sorted);
     });
 };
 
@@ -52,3 +59,30 @@ export const create = (req: Request, res: Response, next: NextFunction) => {
     res.status(201).json(todoList);
   });
 };
+
+/**
+ * Todoリストの配列の要素を、ToDoリストに登録されているToDoの作成日が新しい順に並び替える。
+ * @param todoLists
+ */
+function sortTodoLists(todoLists: ITodoList[]) {
+  const sorted = todoLists.sort((t1, t2) => {
+    const t1FirstTodo = t1.todos[0];
+    const t2FirstTodo = t2.todos[0];
+    if (t1FirstTodo && t2FirstTodo) {
+      if (t1FirstTodo.createdAt < t2FirstTodo.createdAt) {
+        return 1;
+      } else if (t1FirstTodo.createdAt > t2FirstTodo.createdAt) {
+        return -1;
+      } else {
+        return 0;
+      }
+    } else if (t2FirstTodo) {
+      return 1;
+    } else if (t1FirstTodo) {
+      return -1;
+    } else {
+      return 0;
+    }
+  });
+  return sorted;
+}

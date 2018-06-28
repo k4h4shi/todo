56c4d20e80279504cf70dd33462039251b68603f
kotaro takahashi <kotaro.t@k4h4shi.com>
2018-06-28 22:57:00 +0900

    ToDoの名前の一意チェックのスコープを、アプリケーションj単位からToDoリスト単位に変更。

--- src/server/models/Todo.ts
+++ src/server/models/Todo.ts
@@ -1,8 +1,7 @@
 import Mongoose, { Document, Model } from "mongoose";
 import timestamps from "mongoose-timestamp";
-import uniqueValidator from "mongoose-unique-validator";
 
-import { ITodoList } from "./TodoList";
+import TodoList, { ITodoList } from "./TodoList";
 import { Timestamp } from "bson";
 
 export interface ITodo extends Document {
@@ -18,8 +17,11 @@ export const TodoSchema = new Mongoose.Schema({
   name: {
     type: String,
     required: true,
-    unique: true,
-    maxlength: 30
+    maxlength: 30,
+    validate: {
+      isAsync: true,
+      validator: nameValidator
+    }
   },
   due: {
     type: Date,
@@ -36,7 +38,32 @@ export const TodoSchema = new Mongoose.Schema({
 });
 
 TodoSchema.plugin(timestamps);
-TodoSchema.plugin(uniqueValidator);
+
+/**
+ * todoリストの無いのtodoの名前が一位である事をを検証するvalidator
+ * @param _value
+ * @param cb
+ */
+function nameValidator(_value, cb) {
+  const todo = this as ITodo;
+  TodoList.findById(todo.todoList)
+    .populate("todos")
+    .exec((err, todoList) => {
+      if (err) {
+        cb(false, err);
+        return;
+      }
+      // todoが含まれるリストに同じ名前のtodoがあるか
+      const isSameNameExists = todoList.todos.some(t => {
+        return t.name === todo.name;
+      });
+      if (isSameNameExists) {
+        cb(false, "Error, expected `name` to be unique.");
+        return;
+      }
+      cb(true);
+    });
+}
 
 const Todo: Model<ITodo> = Mongoose.model<ITodo>("Todo", TodoSchema);
 

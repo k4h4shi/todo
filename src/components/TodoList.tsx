c3e48369d8e3241edf70ead12e12778e0d458022
kotaro takahashi <kotaro.t@k4h4shi.com>
2018-06-29 14:44:11 +0900

    HeadingやMessageなどのコンポーネントを詳細ページのコンポーネントに移動。

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,46 +1,41 @@
 import React from "react";
 import styled from "styled-components";
-import { Button, FormatDate, Heading, Message } from "../components";
+import { Button, FormatDate, Message } from "../components";
 import { Todo } from "../types";
 import colors from "../config/colors";
 
 interface Props {
-  todoAdded: boolean;
   toggleTodo: (string) => void;
   todos: Todo[];
 }
 
-export default ({ todoAdded, todos, toggleTodo }: Props) => (
+export default ({ todos, toggleTodo }: Props) => (
   <div>
-    <Heading type="title">Todo一覧</Heading>
     {todos.length === 0 ? (
       <Message type="error">登録されたToDoはございません</Message>
     ) : (
-      <div>
-        {todoAdded && <Message type="success">ToDoが追加されました。</Message>}
-        <List>
-          {todos.map((todo, i) => (
-            <ListItem key={i}>
-              <TodoInfo>
-                <h4>{todo.name}</h4>
-                <DateWrapper>
-                  期限: <FormatDate date={todo.due} />
-                </DateWrapper>
-                <DateWrapper>
-                  作成日: <FormatDate date={todo.createdAt} />
-                </DateWrapper>
-              </TodoInfo>
-              <Button
-                type="button"
-                colorType={todo.completed ? "success" : "error"}
-                onClick={() => toggleTodo(todo._id)}
-              >
-                {todo.completed ? "完了" : "未完了"}
-              </Button>
-            </ListItem>
-          ))}
-        </List>
-      </div>
+      <List>
+        {todos.map((todo, i) => (
+          <ListItem key={i}>
+            <TodoInfo>
+              <h4>{todo.name}</h4>
+              <DateWrapper>
+                期限: <FormatDate date={todo.due} />
+              </DateWrapper>
+              <DateWrapper>
+                作成日: <FormatDate date={todo.createdAt} />
+              </DateWrapper>
+            </TodoInfo>
+            <Button
+              type="button"
+              colorType={todo.completed ? "success" : "error"}
+              onClick={() => toggleTodo(todo._id)}
+            >
+              {todo.completed ? "完了" : "未完了"}
+            </Button>
+          </ListItem>
+        ))}
+      </List>
     )}
   </div>
 );
